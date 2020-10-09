e359c58e6431c0691188953fd141f0c1ec8f58bc
1155127307@link.cuhk.edu.hk <longhuenchan@gmail.com>
2020-10-10 00:06:39 +0800

    Update models.py

--- jahk/dashboard/models.py
+++ jahk/dashboard/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     intro = models.CharField(max_length = 500)
-    duration = models.DurationField()
+    duration = models.IntegerField()
     points = models.IntegerField()
     instructor = models.CharField(max_length = 100)
