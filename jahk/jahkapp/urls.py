51afae8d159f27d2def526f78f340857d6f8c981
1155127307@link.cuhk.edu.hk <longhuenchan@gmail.com>
2020-10-10 00:35:35 +0800

    Update urls.py

--- jahk/jahkapp/urls.py
+++ jahk/jahkapp/urls.py
@@ -7,6 +7,6 @@ app_name = "jahkapp"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register/", views.register, name="register"),
-    path("dashboard/", views.dashboard, name="dashboard")
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("login", views.login_request, name="login"),
 ]
