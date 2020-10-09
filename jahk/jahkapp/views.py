15a650be7a7603fe1443a5cda8cd69dafd93a25e
syh <61004856+yuhengfdada@users.noreply.github.com>
2020-10-09 21:03:17 +0800

    more complete registration

--- jahk/jahkapp/views.py
+++ jahk/jahkapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Event
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def homepage(request):
     return render(request = request,
@@ -20,7 +21,7 @@ def register(request):
 
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
             return render(request = request,
                           template_name = "jahkapp/register.html",
